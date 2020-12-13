53b244585df88d65cbc35a38846cd0f87830a9e7
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-13 19:58:44 +0530

    YES

--- index.js
+++ index.js
@@ -8,20 +8,56 @@ var rock_div = document.getElementById('r')
 var paper_div = document.getElementById('p')
 var scissors_div = document.getElementById('s')
 
+const draw = () => {
+    console.log('\n')
+    console.log('You both are quite smart!')
+    console.log('Draw')
+
+}
+
+
+const win= () => {
+    userScore++
+    userScore_span.innerHTML = userScore;
+    // computerScore_span.innerHTML = computerScore_span;
+}
+
+const loose= () => {
+    
+}
+
 
 
 
 const getComputerChoice = () => {
     var choices = ['r','p','s']
-    console.log(Math.floor(Math.random() *3 ))
+    var randNum = Math.floor(Math.random() * 3)
+    return choices[randNum]
 }
-getComputerChoice()
 
 const game = (userChoice) => {
-    console.log(`${userChoice}`)
+    var computerChoice = getComputerChoice()
+    // console.log('Computer Choice => ',computerChoice)
+    // console.log( 'User Choice => ' , userChoice)
+
+    switch (userChoice + computerChoice){
+        case "rp":
+        case 'pr':
+        case 'sp':
+            win(userScore)
+            break;
+        case "rs":
+        case 'ps':
+        case 'sr':
+            loose(userScore)
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            draw()
+
+    }
 }
-
-
 
 
 const main = () => {
