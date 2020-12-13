b2ecfd6b23145a259be0633fd5982c31c8ddcda2
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-13 11:14:38 +0530

    Start Javascript! Add Vars

---
+++ index.js
@@ -0,0 +1,10 @@
+var userScore = 0;
+var computerScore = 0;
+var userScore_span = document.getElementById('user-score');
+var computerScore_span = document.getElementById('computer-score')
+var scoreBoard_div = document.querySelector('.score-board')
+var result_div = document.querySelector('.result')
+var rock_div = document.getElementById('r')
+var paper_div = document.getElementById('p')
+var scissors_div = document.getElementById('s')
+
